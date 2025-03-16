0f32be66a8f2cdb42a8b8ec8f11d9f3f7884f2e9
Ylia Callan 🎼🕸️🪞 <yliacallan@gmail.com>
2025-03-16 11:14:16 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,24 @@
-const cacheName = 'troLab-cache-v1';
+const cacheName = 'trolab-v1';
 const assets = [
-    '/',
-    '/index.html',
-    '/demo.html',
-    '/info.html',
-    '/style.css',
-    '/assets/logo.png',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/TroLab/',
+    '/TroLab/index.html',
+    '/TroLab/demo.html',
+    '/TroLab/info.html',
+    '/TroLab/style.css',
+    '/TroLab/scripts/share.js',
+    '/TroLab/scripts/search.js'
 ];
-
-// Install Service Worker
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(assets);
-        })
-    );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
-
-// Activate Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== cacheName).map(key => caches.delete(key))
-            );
-        })
-    );
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)))));
 });
-
-// Fetch Assets
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(res => {
-            return res || fetch(event.request).then(fetchRes => {
-                return caches.open(cacheName).then(cache => {
-                    cache.put(event.request, fetchRes.clone());
-                    return fetchRes;
-                });
-            });
-        }).catch(() => {
-            return new Response('Offline: You are not connected to the internet.', {
-                status: 503,
-                statusText: 'Service Unavailable'
-            });
-        })
-    );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request).then(fetchRes => {
+        return caches.open(cacheName).then(cache => {
+            cache.put(e.request, fetchRes.clone());
+            return fetchRes;
+        });
+    }).catch(() => new Response('Offline: You are not connected to the internet.', { status: 503, statusText: 'Service Unavailable' }))));
 });
